47fc0acf88df6402cd12a576db1119ff7a4fa46d
Rais4ever <rais_gremory@outlook.com>
2026-08-05 07:25:37 +0000

    new clang for A17
    Change-Id: I7b445ac6c9e3117586167cbb7f9f25d7b06b4c32

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,6 +48,8 @@ BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_SOURCE := kernel/motorola/sm6375
 TARGET_KERNEL_CONFIG := vendor/holi-qgki_defconfig vendor/ext_config/lineage_moto-holi.config
+TARGET_KERNEL_CLANG_VERSION := r563880c
+TARGET_KERNEL_CLANG_PATH := $(abspath .)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
